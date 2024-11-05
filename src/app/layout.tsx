df74beb20f7d4f5033e5dc6e6a27f4ca7a6f8841
JavzaaBatmunkh <javzaabatmunkh@gmail.com>
2024-11-05 18:07:17 +0800

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/components/header";
+import { Suspense } from 'react';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,7 +31,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Header />
+        <Suspense fallback={<div>Loading...</div>}>
         {children}
+        </Suspense>
       </body>
     </html>
   );
